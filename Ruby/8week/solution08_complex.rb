2a560f10b955464ae026ed6012ba247d2ce7f778
Corey Gross <coggro@gmail.com>
2016-11-07 21:24:37 -0600

    Solution 8 Complete

--- Ruby/8week/solution08_complex.rb
+++ Ruby/8week/solution08_complex.rb
@@ -10,19 +10,6 @@ def prompt(message)
     print("\n")
 end
 
-def testName(n)
-    return (n =~ /([A-Z][A-Za-z]*\s?){2}/)
-end
-
-def testPhone(p)
-    return (p =~ /\([0-9]{3}\)[0-9]{3}-[0-9]{4}/)
-end
-
-def testEmail(e)
-    return (e =~ /\w+@\w+\.\w{2,3}/i)
-end
-
-
 $cs = Screen.new
 $cs.cls
 
@@ -50,17 +37,18 @@ def main()
         end
    
         $cs.cls
-        if (!testName(name))
+
+        if (name !~ /([A-Za-z]+\.?\s?)+/)
             puts("Name is invalid.")
         end
-        if (!testPhone(phone))
+        if (phone !~ /\(?\d{3}\)?-?\d{3}-?\d{4}/)
             puts("Phone is invalid.")
         end
-        if (!testEmail(email))
+        if (email !~ /\w+@\w+\.(com|net|org|edu)/i)
             puts("Email is invalid.")
         end
-
-        if (testName(name) && testPhone(phone) && testEmail(email))
+    
+        if (name =~ /([A-Za-z]+\.?\s?)+/ && phone =~ /\(?\d{3}\)?-?\d{3}-?\d{4}/ && email =~ /\w+@\w+\.(com|net|org|edu)/i)
             puts("Everything was entered correctly!")
             valid = true
         else
@@ -70,16 +58,14 @@ def main()
         pause = STDIN.gets
         $cs.cls
         
-        if(valid)
-            output = "User: @name\nPhone: @phone\nEmail: @email".sub("@name", name).sub("@phone", phone).sub("@email", email)
-            puts(output)
-            pause = STDIN.gets
-            $cs.cls
-        end
-
     end
     
+    output = "User: @name\nPhone: @phone\nEmail: @email".sub("@name", name).sub("@phone", phone).sub("@email", email)
+    puts(output)
+
+    pause = STDIN.gets
     $cs.cls
+
 
 end
 
